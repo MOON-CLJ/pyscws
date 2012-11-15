b74327b6b8d3915e5f9bad5c0ddf22f2553e3b1b
MOON_CLJ <lijunli2598@gmail.com>
2012-11-15 10:52:52 +0800

    change py version

--- setup.py
+++ setup.py
@@ -4,23 +4,21 @@
 from distutils.core import setup, Extension
 
 
-scws_module = Extension(name='scws', 
-        sources = ['pyscws.c'], 
-        language='c', 
-#include_dirs=['/usr/include/python2.5', 
-#           '/usr/local/scws/include'], 
-#        library_dirs=['/usr/local/scws/lib'],
-        libraries=['scws'], 
-        extra_compile_args=[])
+scws_module = Extension(name='scws',
+                        sources=['pyscws.c'],
+                        language='c',
+                        include_dirs=['/usr/include/python2.7',
+                                      '/usr/local/scws/include/scws'],
+                        library_dirs=['/usr/local/scws/lib'],
+                        libraries=['scws'],
+                        extra_compile_args=[])
 
 
-setup (
-    name = 'scws',
-    version = '0.0.1',
-    url = 'http://code.google.com/p/pyscws/',
-    description = 'a python package for scws',
-    author_email = 'beijixuexiong@gmail.com, ysj.ray@gmail.com, liangjianglou@126.com',
-    ext_modules = [scws_module]
+setup(
+    name='scws',
+    version='0.0.1',
+    url='http://code.google.com/p/pyscws/',
+    description='a python package for scws',
+    author_email='beijixuexiong@gmail.com, ysj.ray@gmail.com, liangjianglou@126.com',
+    ext_modules=[scws_module]
 )
-
-
