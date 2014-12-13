907749c7bbf325a92d50d6fcc32ce4025bf798c5
MOON_CLJ <lijunli2598@gmail.com>
2014-12-13 20:59:01 +0800

    fix

--- scws_seg.py
+++ scws_seg.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import re
 import scws
 
 SCWS_ENCODING = 'utf-8'
@@ -14,6 +16,15 @@ CUSTOM_DICT_PATH = os.path.join(ABSOLUTE_DICT_PATH, 'userdic.txt')
 EXTRA_STOPWORD_PATH = os.path.join(ABSOLUTE_DICT_PATH, 'stopword.txt')
 EXTRA_EMOTIONWORD_PATH = os.path.join(ABSOLUTE_DICT_PATH, 'emotionlist.txt')
 EXTRA_ONE_WORD_WHITE_LIST_PATH = os.path.join(ABSOLUTE_DICT_PATH, 'one_word_white_list.txt')
+
+
+def load_one_words():
+    one_words = [line.strip('\r\n') for line in file(EXTRA_ONE_WORD_WHITE_LIST_PATH)]
+    return one_words
+
+
+single_word_whitelist = set(load_one_words())
+single_word_whitelist |= set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
 
 
 def load_scws():
@@ -34,25 +45,25 @@ def load_scws():
     return s
 
 
+def _cut_filter(text):
+    pattern_list = [r'\（分享自 .*\）', r'http://t.cn/\w*']
+    for i in pattern_list:
+        p = re.compile(i)
+        text = p.sub('', text)
+    return text
+
+
 def cut(s, text, f=None, cx=False):
     if f:
         tks = [token for token
-               in s.participle(cut_filter(text))
+               in s.participle(_cut_filter(text))
                if token[1] in f and (3 < len(token[0]) < 30 or token[0] in single_word_whitelist)]
     else:
         tks = [token for token
-               in s.participle(cut_filter(text))
+               in s.participle(_cut_filter(text))
                if 3 < len(token[0]) < 30 or token[0] in single_word_whitelist]
 
     if cx:
         return tks
     else:
         return [tk[0] for tk in tks]
-
-
-def cut_filter(text):
-    pattern_list = [r'\（分享自 .*\）', r'http://t.cn/\w*']
-    for i in pattern_list:
-        p = re.compile(i)
-        text = p.sub('', text)
-    return text
